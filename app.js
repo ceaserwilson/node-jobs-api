6bc66689e7aecccbcafd5aea34de5c65c35eb88d
ceaserwilson <christianwilsonobi@gmail.com>
2024-06-11 08:42:04 +0100

    added swagger docs

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // connect DB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
@@ -39,8 +44,9 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.get('/', (req, res) => {
-  res.send(`<h2>welcome to jobs API</h2>`)
+  res.send(`<h2>welcome to jobs API</h2><a href="/api-docs">Documentation</a>`)
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
